092dae17650916dc246e723831a20edc793a4f6d
Danielle R <60155705+dreisch111@users.noreply.github.com>
2023-04-06 08:55:18 -0400

    Remove old files

--- brand-style-updates.js
+++
@@ -1,9 +0,0 @@
-/* Fix case of LATEST ISSUE link */
-$(".cover-image__link").text("Latest Issue");
-
-/* Fix case of Submit an aritcle link */
-$("#SubmitanArticle a").text("Submit an article");
-$("#SubmitanArticle a").prepend('<i aria-hidden="true" class="icon-submit_article"></i>');
-
-/* Fix More articles link */
-$(".more-widget-link").text("More articles");
